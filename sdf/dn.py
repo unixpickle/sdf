7151e5974da51ebc811777113e27fbfe6e076c48
Alex Nichol <unixpickle@gmail.com>
2021-02-18 10:17:30 -0500

    torch dn.py

--- sdf/dn.py
+++ sdf/dn.py
@@ -1,87 +1,104 @@
 import itertools
-import numpy as np
+import torch
 
-_min = np.minimum
-_max = np.maximum
+_min = torch.minimum
+_max = torch.maximum
+
 
 def union(a, *bs, k=None):
     def f(p):
         d1 = a(p)
         for b in bs:
             d2 = b(p)
-            K = k or getattr(b, '_k', None)
+            K = k or getattr(b, "_k", None)
             if K is None:
                 d1 = _min(d1, d2)
             else:
-                h = np.clip(0.5 + 0.5 * (d2 - d1) / K, 0, 1)
+                h = (0.5 + 0.5 * (d2 - d1) / K).clamp(0, 1)
                 m = d2 + (d1 - d2) * h
                 d1 = m - K * h * (1 - h)
         return d1
+
     return f
+
 
 def difference(a, *bs, k=None):
     def f(p):
         d1 = a(p)
         for b in bs:
             d2 = b(p)
-            K = k or getattr(b, '_k', None)
+            K = k or getattr(b, "_k", None)
             if K is None:
                 d1 = _max(d1, -d2)
             else:
-                h = np.clip(0.5 - 0.5 * (d2 + d1) / K, 0, 1)
+                h = (0.5 - 0.5 * (d2 + d1) / K).clamp(0, 1)
                 m = d1 + (-d2 - d1) * h
                 d1 = m + K * h * (1 - h)
         return d1
+
     return f
+
 
 def intersection(a, *bs, k=None):
     def f(p):
         d1 = a(p)
         for b in bs:
             d2 = b(p)
-            K = k or getattr(b, '_k', None)
+            K = k or getattr(b, "_k", None)
             if K is None:
                 d1 = _max(d1, d2)
             else:
-                h = np.clip(0.5 - 0.5 * (d2 - d1) / K, 0, 1)
+                h = (0.5 - 0.5 * (d2 - d1) / K).clamp(0, 1)
                 m = d2 + (d1 - d2) * h
                 d1 = m + K * h * (1 - h)
         return d1
+
     return f
+
 
 def blend(a, *bs, k=0.5):
     def f(p):
         d1 = a(p)
         for b in bs:
             d2 = b(p)
-            K = k or getattr(b, '_k', None)
+            K = k or getattr(b, "_k", None)
             d1 = K * d2 + (1 - K) * d1
         return d1
+
     return f
+
 
 def negate(other):
     def f(p):
         return -other(p)
+
     return f
+
 
 def dilate(other, r):
     def f(p):
         return other(p) - r
+
     return f
+
 
 def erode(other, r):
     def f(p):
         return other(p) + r
+
     return f
+
 
 def shell(other, thickness):
     def f(p):
-        return np.abs(other(p)) - thickness / 2
+        return other(p).abs() - thickness / 2
+
     return f
 
+
 def repeat(other, spacing, count=None, padding=0):
-    count = np.array(count) if count is not None else None
-    spacing = np.array(spacing)
+    count_th = torch.tensor(count) if count is not None else None
+    spacing_th = torch.tensor(spacing)
 
     def neighbors(dim, padding, spacing):
         try:
@@ -99,16 +116,18 @@ def repeat(other, spacing, count=None, padding=0):
         return list(itertools.product(*axes))
 
     def f(p):
-        q = np.divide(p, spacing, out=np.zeros_like(p), where=spacing != 0)
-        if count is None:
-            index = np.round(q)
+        q = p / torch.where(
+            spacing_th == 0, torch.ones_like(spacing_th), spacing_th
+        ).to(p)
+        if count_th is None:
+            index = q.round()
         else:
-            index = np.clip(np.round(q), -count, count)
+            count_dev = count_th.to(index.device)
+            index = q.round().clamp(-count_dev, count_dev)
 
-        indexes = [index + n for n in neighbors(p.shape[-1], padding, spacing)]
-        A = [other(p - spacing * i) for i in indexes]
-        a = A[0]
-        for b in A[1:]:
-            a = _min(a, b)
-        return a
+        offsets = neighbors(p.shape[-1], padding, spacing)
+        indices = torch.cat([index + n for n in offsets])
+        A = other(p - spacing_th.to(p) * indices).view(len(offsets), -1)
+        return A.min(0)
+
     return f
